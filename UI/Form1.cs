0d234d49b05882ec82c69178b7b56ef8691159a9
Dylan Jasper <dzj0034@auburn.edu>
2019-09-09 15:55:29 -0700

    Added icons to the toolstrip.

--- UI/Form1.cs
+++ UI/Form1.cs
@@ -26,5 +26,25 @@ namespace UI
         {
             MessageBox.Show("This is a fully functioning web browser application built in Software Construction Fundamentals, 2019.");
         }
+
+        private void WebBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+        }
+
+        private void AddressBox_Click(object sender, EventArgs e)
+        {
+            addressBox.Text = "";
+        }
+
+        private void ToolStripButton6_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate(addressBox.Text.ToString());
+
+        }
+
+        private void ToolStripButton2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
